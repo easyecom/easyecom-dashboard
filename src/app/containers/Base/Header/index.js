d9cd73c9c8ce4144c679d823db5bcecd890ca615
thi777 <thiagocarvalho.ads@gmail.com>
2020-09-27 12:27:41 -0300

    wip style(order):  creating header style

--- src/app/containers/Base/Header/index.js
+++ src/app/containers/Base/Header/index.js
@@ -5,15 +5,21 @@ import { Container } from "./styles";
 const Header = () => {
   return (
     <Container>
-      <div className="flex x-axis full-width">
+      <div className="flex x-axis full-width header-box">
         <div className="flex-1 flex flex-start">
-          <a href="/" className="text">Ver loja</a>
+          <a href="/" className="text see-store-header">
+            Ver loja
+          </a>
+          <i className="fas fa-store" />
         </div>
-        <div className="flex-1 flex flex-end">
-          <Link to="Logout" className="text">Sair</Link>
+        <div className="flex-1 flex flex-end logout-store-header">
+          <Link to="Logout" className="text">
+            Sair
+          </Link>
+          <i className="fas fa-power-off" />
         </div>
       </div>
-      <hr/>
+      {/* <hr color="#cc8400" size="1" width="50%" text-align="left"/> */}
     </Container>
   );
 };
--- src/app/containers/Base/Header/styles.js
+++ src/app/containers/Base/Header/styles.js
@@ -2,12 +2,39 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   width: 100%;
-  max-width: 100%;
-  padding: 32px 0 0 0px;
+  padding: 0 0 0 0px;
+  height: 70px;
+  border-bottom: 1px dotted #cc8400;
+
+  .header-box {
+    height: 100%;
+  }
+
+  .see-store-header {
+    padding: 0 5px 0 20px;
+  }
+
+  .fa-store {
+    color: #3895d3;
+  }
+
+  .logout-store-header {
+    padding-right: 20px;
+    color: red;
+    
+    .fa-power-off {
+      padding-left: 5px;
+      color: red;
+    }
+  }
 
   .text {
     color: #000;
-    padding: 0 20px 12px 20px;
+    font-weight: bold;
+
+    .fas-store-header: {
+      margin-right: 15px;
+    }
   }
 
   a {
